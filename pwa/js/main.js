f388aa09e0658a42ca44311cf96dfe39fef6a1ee
unknown <rueychi.jao@gmail.com>
2020-11-13 11:06:57 +0800

    sd

--- pwa/js/main.js
+++ pwa/js/main.js
@@ -1,6 +1,11 @@
-window.onload = () => {
- 
-  if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('pwa/sw.js', {scope: '/'})
-  }  
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('pwa/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
