75ab310634bbab8a24aa52bef2c31611225c456b
unknown <rueychi.jao@gmail.com>
2020-11-13 10:45:13 +0800

    second

--- pwa/js/main.js
+++ pwa/js/main.js
@@ -2,6 +2,6 @@ window.onload = () => {
   'use strict';
   
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/pwa/sw.js');
+    navigator.serviceWorker.register('pwa/sw.js');
   }
 }
